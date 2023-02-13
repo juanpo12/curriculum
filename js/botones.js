6b469f970acd930fdc021fde4fb02953cab0c9a0
Juan Diaz <juanjoc2000@hotmail.com>
2023-02-13 18:19:29 -0300

    finalizado

---
+++ js/botones.js
@@ -0,0 +1,16 @@
+document.getElementById("botonInstagram").onclick = function (){
+    location.href="https://www.instagram.com/tomholland2013/"
+}
+
+document.getElementById("botonFacebook").onclick = function (){
+    location.href="https://www.facebook.com/groups/3467452986671727/"
+}
+
+document.getElementById("botonTwitter").onclick = function (){
+    location.href="https://twitter.com/TomHolland1996"
+}
+
+document.getElementById("botonLinkedin").onclick = function (){
+    location.href="https://ar.linkedin.com/"
+}
+
